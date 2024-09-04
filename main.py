670f68ecbbbfc7b0462495ad1075ca870ad13f81
nirvik34 <jarhnirvik@gmail.com>
2024-09-04 22:03:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,17 +2,13 @@ import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from datetime import datetime
-
-# List of people with their birthdays
 people = [
     {"name": "ABC", "email": "XYZ@gmail.com", "birthday": "08-08"},
 ]
 
-# Email credentials
 sender_email = "senders email"
 sender_password = "valid password"
 
-# Function to send email
 def send_email(to_email, subject, body):
     msg = MIMEMultipart()
     msg['From'] = sender_email
@@ -31,10 +27,7 @@ def send_email(to_email, subject, body):
     except Exception as e:
         print(f"Failed to send email: {e}")
 
-# Checking Date
 today = datetime.today().strftime("%m-%d")
-
-# Check birthdays and send email if today is someone's birthday
 for person in people:
     if person["birthday"] == today:
         subject = f"All the best, {person['name']}!"
